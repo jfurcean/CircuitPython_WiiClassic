1dc0638972655265dc5b772ba3ef6936cc087d93
John Furcean <furceanj@msu.edu>
2021-01-11 15:52:50 -0500

    run black

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     ],
     # What does your project relate to?
     keywords="adafruit blinka circuitpython micropython wiiclassic Wii Nunchuck "
-             "ClassicController Classic Controller",
+    "ClassicController Classic Controller",
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # TODO: IF LIBRARY FILES ARE A PACKAGE FOLDER,
